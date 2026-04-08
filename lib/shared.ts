26fbe94a72933f29a97a647f8d8bc2eacc063400
tcitry <tcitry@gmail.com>
2026-04-08 16:46:38 +0800

    chore: fix description.

--- lib/shared.ts
+++ lib/shared.ts
@@ -4,7 +4,7 @@ export const docsImageRoute = '/og';
 export const docsContentRoute = '/llms.mdx';
 
 export const gitConfig = {
-  user: 'nicely-gg',
-  repo: 'scalebox-fumadocs',
+  user: 'scalebox-dev',
+  repo: 'scalebox-docs',
   branch: 'main',
 };
